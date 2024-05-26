677f4efaa25f822e3e76aca76bd648efb31bc1a7
MARIEM-AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-05-26 18:33:30 +0100

    Add files via upload

---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        your first name is <?php echo $_GET['FN']; ?><br>
+        your last name is :  <?php echo $_GET['LN']; ?>
+</body>
+</html>
